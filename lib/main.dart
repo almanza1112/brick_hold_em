2d986c1dd75dd804add9b8026be696cfc06a2714
almanza1112 <almanza1112@gmail.com>
2022-11-13 04:06:50 -0500

    added images/graphics, implemented stable sprites

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
@@ -13,8 +15,36 @@ void main() async {
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.landscapeLeft,
     DeviceOrientation.landscapeRight
-  ]).then((value) => runApp(MyApp()));
+  ]).then((value) => runApp(GameWidget(game: MyGame())));
 }
+
+class MyGame extends FlameGame {
+
+  SpriteComponent background = SpriteComponent();
+  SpriteComponent twoClubs = SpriteComponent();
+
+  final double cardHeight = 100.0;
+  final double cardWidth = 50.0;
+
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+
+    final screenWidth = size[0];
+    final screenHeight = size[1];
+
+    add(background
+      ..sprite = await loadSprite('background.webp')
+      ..size = size);
+
+    twoClubs
+      ..sprite = await loadSprite('2_of_clubs.png')
+      ..size = Vector2(cardWidth, cardHeight)
+      ..y = 100;
+    add(twoClubs);
+  }
+}
+
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
