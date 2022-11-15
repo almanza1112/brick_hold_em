f4211ef7ca485097151e42bf5f68aa7e0968da2b
almanza1112 <almanza1112@gmail.com>
2022-11-15 04:41:30 -0500

    added exit button (no functionality)

--- lib/game_page.dart
+++ lib/game_page.dart
@@ -1,13 +1,21 @@
+import 'package:flame/input.dart';
+import 'package:flame/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 
-class GamePage extends FlameGame {
+class GamePage extends FlameGame with HasTappables{
   SpriteComponent background = SpriteComponent();
   SpriteComponent twoClubs = SpriteComponent();
+  ExitButton exitButton = ExitButton();
+  final Vector2 exitButtonSize = Vector2(50.0, 30.0);
+  
 
   final double cardHeight = 70.0;
   final double cardWidth = 40.0;
+
+  TextPaint sampleText = TextPaint(style: const TextStyle(fontSize: 18));
+
 
   @override
   Future<void> onLoad() async {
@@ -26,6 +34,13 @@ class GamePage extends FlameGame {
       ..y = screenHeight - cardHeight
       ..x = screenWidth - cardWidth;
     add(twoClubs);
+
+    exitButton
+    ..sprite = await loadSprite('exit.png')
+    ..size = exitButtonSize
+    ..position = Vector2(10, 10);
+    add(exitButton);
+    
   }
 
   @override
@@ -34,6 +49,27 @@ class GamePage extends FlameGame {
 
     if (twoClubs.y > size[1] / 2) {
       twoClubs.y -= 30 * dt;
+    }
+  }
+
+  @override
+  void render(Canvas canvas){
+    super.render(canvas);
+    sampleText.render(canvas, "Sample Text", Vector2(size[0] / 2, 10));
+  }
+}
+
+class ExitButton extends SpriteComponent with Tappable {
+
+  @override
+  bool onTapDown(TapDownInfo event){
+    try {
+      print("object");
+      
+      return true;
+    } catch (error){
+      print(error);
+      return false;
     }
   }
 }
