60d90c91a89de9b0a2191a2d18555837bb485db2
almanza1112 <almanza1112@gmail.com>
2022-11-22 03:30:11 -0500

    sign in flow update

--- lib/auth_service.dart
+++ lib/auth_service.dart
@@ -3,6 +3,7 @@ import 'package:flame/game.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'home_page.dart';
 import 'login_page.dart';
@@ -36,7 +37,33 @@ class AuthService {
     );
 
     // Once signed in, return the UserCredential
-    return await FirebaseAuth.instance.signInWithCredential(credential);
+    var userCred = await FirebaseAuth.instance.signInWithCredential(credential);
+
+
+    String uid = FirebaseAuth.instance.currentUser!.uid;
+
+    FirebaseFirestore db = FirebaseFirestore.instance;
+
+    FirebaseFirestore.instance
+      .collection('users')
+      .doc(uid)
+      .get()
+      .then((DocumentSnapshot documentSnapshot) {
+        if(documentSnapshot.exists){
+          return userCred;
+        } else {
+
+          db.collection("users")
+            .doc(uid)
+            .set({
+              'coins': 1000
+            })
+            .then((value) => print("User Added"))
+            .catchError((error) => print("Failed to add user: $error"));
+        }
+      });
+
+    return userCred;
   }
 
   Future<UserCredential> signInWithFacebook() async {
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,11 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'settings_page.dart';
 import 'game_page.dart';
-
 
 class HomePage extends StatefulWidget {
   @override
@@ -12,10 +13,36 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // Create a CollectionReference called users that references the firestore collection
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+  Future<void> addUser() {
+    // Call the user's CollectionReference to add a new user
+    return users
+        .add({'firstName': 'Bryant', 'lastName': 'Almanza', 'age': '29'})
+        .then((value) => print("User Added"))
+        .catchError((error) => print("Failed to add user: $error"));
+  }
+
+  var docId = '62XgoUPzHzUHtedwibyL';
+  
+  readData(){
+    return FirebaseFirestore.instance
+      .collection('users')
+      .doc(docId)
+      .get()
+      .then((DocumentSnapshot documentSnapshot) {
+        if(documentSnapshot.exists) {
+          print("document does exist! yayy!!");
+        } else {
+          print("youre wack!");
+        }
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     
       body: Container(
         color: Colors.brown,
         width: MediaQuery.of(context).size.width,
@@ -25,61 +52,60 @@ class _HomePageState extends State<HomePage> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 Expanded(
-                  child: Container(
-                    alignment: Alignment.centerLeft,
-                    child: IconButton(
+                    child: Container(
+                  alignment: Alignment.centerLeft,
+                  child: IconButton(
                       onPressed: () {}, icon: const Icon(Icons.group)),
-                  ) 
-                ),
-                
+                )),
                 Expanded(
-                  child: Container(
-                    alignment: Alignment.center,
-                    child: const Text('1000 chips')
-                  )
-                ),
-              
+                    child: Container(
+                        alignment: Alignment.center,
+                        child: const Text('1000 chips'))),
                 Expanded(
-                  child: Container(
-                    alignment: Alignment.centerRight,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: <Widget>[
-                        IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.local_movies)
-                        ),
-                        IconButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context, 
-                              MaterialPageRoute(builder: (context) =>  SettingsPage()));
-                          },
-                          icon: const Icon(Icons.settings)
-                        )
-                      ],
-                    )
-                  )
-                ),
+                    child: Container(
+                        alignment: Alignment.centerRight,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.local_movies)),
+                            IconButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              SettingsPage()));
+                                },
+                                icon: const Icon(Icons.settings))
+                          ],
+                        ))),
               ],
             ),
-
-            MaterialButton(
-                    onPressed: () {
-                       Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => GameWidget(game: GamePage())));
-                    },
-                    padding: const EdgeInsets.all(16.0),
-                    color: Colors.green,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5)),
-                    child: const Text(
-                      "Start Playing",
-                      style: TextStyle(color: Colors.white, fontSize: 20),),
-                  ),
             MaterialButton(
               onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => GameWidget(game: GamePage())));
+              },
+              padding: const EdgeInsets.all(16.0),
+              color: Colors.green,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5)),
+              child: const Text(
+                "Start Playing",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
 
+            /* TEST BUTTON TODO: remove this eventaully */
+            MaterialButton(
+              onPressed: () {
+                //addUser();
+                //print(FirebaseAuth.instance.currentUser!.uid);
+                readData();
               },
               color: Colors.yellow,
               shape: RoundedRectangleBorder(
@@ -90,7 +116,6 @@ class _HomePageState extends State<HomePage> {
                 style: TextStyle(color: Colors.black),
               ),
             )
-
           ],
         ),
       ),
