0ff48dedcb6e3092600a66efae9849765e45b037
almanza1112 <almanza1112@gmail.com>
2022-11-27 05:42:08 -0500

    game update

--- lib/game_page.dart
+++ lib/game_page.dart
@@ -3,19 +3,34 @@ import 'package:flame/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class GamePage extends FlameGame with HasTappables{
   SpriteComponent background = SpriteComponent();
   SpriteComponent twoClubs = SpriteComponent();
+  SpriteComponent eightHearts = SpriteComponent();
+  SpriteComponent brickCard = SpriteComponent();
+  SpriteComponent fourSpade = SpriteComponent();
+  SpriteComponent aceDiamond = SpriteComponent();
+  SpriteComponent player2_bs1 = SpriteComponent();
+  SpriteComponent player2_bs2 = SpriteComponent();
+  SpriteComponent player2_bs3 = SpriteComponent();
+  SpriteComponent player2_bs4 = SpriteComponent();
+  SpriteComponent player2_bs5 = SpriteComponent();
+  SpriteComponent player3_bs1 = SpriteComponent();
+  SpriteComponent player3_bs2 = SpriteComponent();
+  SpriteComponent player3_bs3 = SpriteComponent();
+  SpriteComponent player3_bs4 = SpriteComponent();
+  SpriteComponent player3_bs5 = SpriteComponent();
+
+  
   ExitButton exitButton = ExitButton();
   final Vector2 exitButtonSize = Vector2(50.0, 30.0);
   
-
-  final double cardHeight = 70.0;
+  final double cardHeight = 60.0;
   final double cardWidth = 40.0;
 
   TextPaint sampleText = TextPaint(style: const TextStyle(fontSize: 18));
-
 
   @override
   Future<void> onLoad() async {
@@ -28,34 +43,145 @@ class GamePage extends FlameGame with HasTappables{
       ..sprite = await loadSprite('background.webp')
       ..size = size);
 
-    twoClubs
-      ..sprite = await loadSprite('2_of_clubs.png')
+    exitButton
+      ..sprite = await loadSprite('exit.png')
+      ..size = exitButtonSize
+      ..position = Vector2(10, 10);
+    add(exitButton);
+
+    // PLAYER 1
+    brickCard
+      ..sprite = await loadSprite('brick_card.png')
       ..size = Vector2(cardWidth, cardHeight)
       ..y = screenHeight - cardHeight
-      ..x = screenWidth - cardWidth;
+      ..x = screenWidth / 2.2;
+    add(brickCard);
+
+    eightHearts
+      ..sprite = await loadSprite('hearts_8.png')
+      ..size = Vector2(cardWidth, cardHeight)
+      ..y = screenHeight - cardHeight
+      ..x = screenWidth / 2.1 ;
+    add(eightHearts);
+
+    twoClubs
+      ..sprite = await loadSprite('club_2.png')
+      ..size = Vector2(cardWidth, cardHeight)
+      ..y = screenHeight - cardHeight
+      ..x = screenWidth / 2;
     add(twoClubs);
 
-    exitButton
-    ..sprite = await loadSprite('exit.png')
-    ..size = exitButtonSize
-    ..position = Vector2(10, 10);
-    add(exitButton);
+    fourSpade
+      ..sprite = await loadSprite('spade_4.png')
+      ..size = Vector2(cardWidth, cardHeight)
+      ..y = screenHeight - cardHeight
+      ..x = screenWidth / 1.9;
+    add(fourSpade);
+
+    aceDiamond
+      ..sprite = await loadSprite('diamond_a.png')
+      ..size = Vector2(cardWidth, cardHeight)
+      ..y = screenHeight - cardHeight
+      ..x = screenWidth / 1.8;
+    add(aceDiamond);
     
+
+    // PLAYER 2
+    player2_bs1
+      ..sprite = await loadSprite('backside.png')
+      ..size = Vector2(cardWidth, cardHeight)
+      ..y = screenHeight / 2.2
+      ..x = cardWidth
+      ..angle = radians(270);
+    add(player2_bs1);
+    
+    player2_bs2
+      ..sprite = await loadSprite('backside.png')
+      ..size = Vector2(cardWidth, cardHeight)
+      ..y = screenHeight / 2.1
+      ..x = cardWidth
+      ..angle = radians(270);
+    add(player2_bs2);
+
+    player2_bs3
+      ..sprite = await loadSprite('backside.png')
+      ..size = Vector2(cardWidth, cardHeight)
+      ..y = screenHeight / 2
+      ..x = cardWidth
+      ..angle = radians(270);
+    add(player2_bs3);
+
+    player2_bs4
+      ..sprite = await loadSprite('backside.png')
+      ..size = Vector2(cardWidth, cardHeight)
+      ..y = screenHeight / 1.9
+      ..x = cardWidth
+      ..angle = radians(270);
+    add(player2_bs4);
+
+    player2_bs5
+      ..sprite = await loadSprite('backside.png')
+      ..size = Vector2(cardWidth, cardHeight)
+      ..y = screenHeight / 1.8
+      ..x = cardWidth
+      ..angle = radians(270);
+    add(player2_bs5);
+
+    // PLAYER 3
+    player3_bs1
+      ..sprite = await loadSprite('backside.png')
+      ..size = Vector2(cardWidth, cardHeight)
+      ..y = screenHeight / 2.5
+      ..x = screenWidth - cardWidth
+      ..angle = radians(90);
+    add(player3_bs1);
+
+    player3_bs2
+      ..sprite = await loadSprite('backside.png')
+      ..size = Vector2(cardWidth, cardHeight)
+      ..y = screenHeight / 2.4
+      ..x = screenWidth - cardWidth
+      ..angle = radians(90);
+    add(player3_bs2);
+
+    player3_bs3
+      ..sprite = await loadSprite('backside.png')
+      ..size = Vector2(cardWidth, cardHeight)
+      ..y = screenHeight / 2.3
+      ..x = screenWidth - cardWidth
+      ..angle = radians(90);
+    add(player3_bs3);
+
+    player3_bs4
+      ..sprite = await loadSprite('backside.png')
+      ..size = Vector2(cardWidth, cardHeight)
+      ..y = screenHeight / 2.2
+      ..x = screenWidth - cardWidth
+      ..angle = radians(90);
+    add(player3_bs4);
+
+    player3_bs5
+      ..sprite = await loadSprite('backside.png')
+      ..size = Vector2(cardWidth, cardHeight)
+      ..y = screenHeight / 2.1
+      ..x = screenWidth - cardWidth
+      ..angle = radians(90);
+    add(player3_bs5);
   }
 
   @override
   void update(double dt) {
     super.update(dt);
 
-    if (twoClubs.y > size[1] / 2) {
-      twoClubs.y -= 30 * dt;
-    }
+    //if (twoClubs.y > size[1] / 3) {
+      //twoClubs.y -= 30 * dt;
+    //}
   }
 
   @override
   void render(Canvas canvas){
     super.render(canvas);
-    sampleText.render(canvas, "Sample Text", Vector2(size[0] / 2, 10));
+    sampleText.render(canvas, "Table 1", Vector2(size[0] / 2, 10));
   }
 }
 
