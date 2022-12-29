91fc962294ec2a9e0a53c55d6c07e5fb7489dc74
almanza1112 <almanza1112@gmail.com>
2022-12-29 15:11:24 -0500

    right side menu

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -17,60 +17,57 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  
   @override
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
 
     return Scaffold(
-          backgroundColor: Colors.white,
-          body: SafeArea(
+        backgroundColor: Colors.orangeAccent,
+        body: SafeArea(
             child: Material(
-              color: Colors.white,
-              child: Stack(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.all(20),
-                    child: Image(image: AssetImage('assets/images/AlmanzaTechLogo.png')),
-                  ),
-                  Align(
-                    alignment: Alignment.bottomLeft,
-                    child: leftMenu(),
-                  )
-                ],
-                
+          color: Colors.orangeAccent,
+          child: Stack(
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(20),
+                child: Image(
+                    image: AssetImage('assets/images/AlmanzaTechLogo.png')),
               ),
-            )
-          )
-          
-        );
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: leftMenu(),
+              ),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: rightMenu(),
+              )
+            ],
+          ),
+        )));
   }
 
-  Widget leftMenu(){
+  Widget leftMenu() {
     return Column(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
         Padding(
-          padding: const EdgeInsets.only(top: 10, bottom: 10),
-          child: Container(
-            decoration: const BoxDecoration(
-              color: Colors.black,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black,
-                  blurRadius: 8,
-                  offset: Offset(0, 4)
-                )
-              ]
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 15),
-              child: IconButton(
-              onPressed: () {}, 
-              color: Colors.white,
-              icon: const Icon(Icons.local_movies_outlined)),)
-          )
-        ),
+            padding: const EdgeInsets.only(top: 10, bottom: 10),
+            child: Container(
+                decoration: const BoxDecoration(
+                    color: Colors.black,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 8,
+                          offset: Offset(0, 4))
+                    ]),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 15),
+                  child: IconButton(
+                      onPressed: () {},
+                      color: Colors.white,
+                      icon: const Icon(Icons.local_movies_outlined)),
+                ))),
         Padding(
             padding: const EdgeInsets.only(top: 10, bottom: 10),
             child: Container(
@@ -79,14 +76,13 @@ class _HomePageState extends State<HomePage> {
                       color: Colors.black, blurRadius: 8, offset: Offset(0, 4))
                 ]),
                 child: Padding(
-                  padding:const  EdgeInsets.only(left: 15),
+                  padding: const EdgeInsets.only(left: 15),
                   child: IconButton(
                       onPressed: () {},
                       color: Colors.white,
-                      icon: const Icon(Icons.shopping_bag)
-                      ),
+                      icon: const Icon(Icons.shopping_bag)),
                 ))),
-          Padding(
+        Padding(
             padding: const EdgeInsets.only(top: 10, bottom: 10),
             child: Container(
                 decoration: const BoxDecoration(color: Colors.blue, boxShadow: [
@@ -100,14 +96,129 @@ class _HomePageState extends State<HomePage> {
                       color: Colors.white,
                       icon: const Icon(Icons.group)),
                 )))
-        
       ],
     );
   }
 
-  
+  Widget rightMenu() {
+    const double height = 90;
+    const double width = 180;
+    const double fontSize = 16;
 
-  Future<Map<String , dynamic>?> readUser() async {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        InkWell(
+            splashColor: Colors.black,
+            onTap: () {},
+            child: Ink(
+              height: height,
+              width: width,
+              decoration: const BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      bottomLeft: Radius.circular(10)),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black,
+                        blurRadius: 8,
+                        offset: Offset(0, 4))
+                  ]),
+              child: const Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "COMPETITVE",
+                  style: TextStyle(fontSize: fontSize, color: Colors.white, fontStyle: FontStyle.italic),
+                ),
+              ),
+            )),
+        // FRIENDLY BUTTON
+        InkWell(
+            splashColor: Colors.black,
+            onTap: () {},
+            child: Ink(
+              height: height,
+              width: width,
+              decoration: const BoxDecoration(
+                  color: Colors.teal,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      bottomLeft: Radius.circular(10)),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black,
+                        blurRadius: 8,
+                        offset: Offset(0, 4))
+                  ]),
+              child: const Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "FRIENDLY",
+                  style: TextStyle(fontSize: fontSize, color: Colors.white, fontStyle: FontStyle.italic),
+                ),
+              ),
+            )),
+        // HOW TO PLAY button
+        InkWell(
+            splashColor: Colors.black,
+            onTap: () {},
+            child: Ink(
+              height: height,
+              width: width,
+              decoration: const BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      bottomLeft: Radius.circular(10)),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black,
+                        blurRadius: 8,
+                        offset: Offset(0, 4))
+                  ]),
+              child: const Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "HOW TO PLAY",
+                  style: TextStyle(fontSize: fontSize, color: Colors.white, fontStyle: FontStyle.italic),
+                ),
+              ),
+            )),
+        // SETTINGS button
+        InkWell(
+            splashColor: Colors.white,
+            onTap: () {},
+            child: Ink(
+              height: height,
+              width: width,
+              decoration: const BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      bottomLeft: Radius.circular(10)
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black,
+                        blurRadius: 8,
+                        offset: Offset(0, 4))
+                  ]
+
+              ),
+              child: const Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "SETTINGS",
+                  style: TextStyle(fontSize: fontSize, color: Colors.white, fontStyle: FontStyle.italic),
+                ),
+              ),
+            )),
+      ],
+    );
+  }
+
+  Future<Map<String, dynamic>?> readUser() async {
     final docUser = FirebaseFirestore.instance
         .collection("users")
         .doc(FirebaseAuth.instance.currentUser!.uid);
@@ -120,6 +231,4 @@ class _HomePageState extends State<HomePage> {
       return {"chips": '20'};
     }
   }
-
-
 }
