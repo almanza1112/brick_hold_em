dca84a724b93727b9c56d71de8e547a9072ea839
almanza1112 <almanza1112@gmail.com>
2023-03-10 22:58:20 -0500

    fail

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 
+import 'package:brick_hold_em/home_page.dart';
+import 'package:brick_hold_em/login_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
@@ -19,14 +22,26 @@ void main() async {
 
 
 
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.system,
       debugShowCheckedModeBanner: false,
-      home: AuthService().handleAuthState(),
+      home: StreamBuilder(
+                stream: FirebaseAuth.instance.authStateChanges(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return HomePage();
+                  } else {
+                    return const LoginPage();
+                  }
+                },
+
+      ),
     );
   }
 }
