66ea2249cd324663dd3ce78f4c0ea564681ea294
almanza1112 <almanza1112@gmail.com>
2023-05-17 18:10:06 -0400

    optmizing and cleaning

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
+import 'package:brick_hold_em/game/game_cards.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:firebase_auth/firebase_auth.dart';
@@ -11,6 +12,7 @@ import 'package:flutter/services.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:brick_hold_em/globals.dart' as globals;
 
+import 'card_key.dart';
 import 'game_sidemenu.dart';
 
 class GamePage extends StatefulWidget {
@@ -34,26 +36,21 @@ extension GlobalPaintBounds on BuildContext {
 }
 
 class GamePageState extends State<GamePage> {
-  final onceRef = FirebaseDatabase.instance.ref();
   final uid = FirebaseAuth.instance.currentUser!.uid;
   late String faceUpCardName;
   Matrix4 playersCardsTransform = Matrix4.translationValues(0, 0, 2);
 
-  late Future<List<String>> _cardsSnapshot;
-  //late Future<String> _getFaceUpCard;
-  List<Widget> tappedCards = <Widget>[];
+  //List<Widget> tappedCards = <Widget>[];
 
   double cardWidth = 50;
   double cardHeight = 70;
   //var cardWidgets = <Widget>[];
   late double constrainHeight, constrainWidth;
-  var cardWidgetsBuilderList = <Widget>[];
+  //var cardWidgetsBuilderList = <Widget>[];
 
-  bool isStateChanged = false;
   bool isYourTurn = false;
 
-  Duration tableCardAnimationDuration = const Duration(milliseconds: 500);
-  bool playButtonSelected = false;
+
 
   DatabaseReference turnOrderListener =
       FirebaseDatabase.instance.ref('tables/1/turnOrder/turnPlayer');
@@ -70,8 +67,6 @@ class GamePageState extends State<GamePage> {
 
   @override
   void initState() {
-    _cardsSnapshot = cardsSnapshot();
-    //_getFaceUpCard = getFaceUpCard();
     addUserToTable();
     super.initState();
   }
@@ -84,9 +79,8 @@ class GamePageState extends State<GamePage> {
         //fit: StackFit.expand,
         children: [
           GamePlayers(),
-          playerCards(),
+          GameCards(),
           deck(),
-          fiveCardBorders(),
           faceUpCard(),
           buttons(),
           GameSideMenu(),
@@ -193,126 +187,6 @@ class GamePageState extends State<GamePage> {
     });
   }
 
-  Widget playerCards() {
-    return SafeArea(
-      child: Stack(children: [
-        Positioned(
-          right: 0,
-          bottom: 50,
-          left: 0,
-          child: FutureBuilder<List<String>>(
-              future: _cardsSnapshot,
-              builder: ((context, snapshot) {
-                if (snapshot.hasData) {
-                  var cardsList = List<String>.from(snapshot.data as List);
-
-                  // Not having the line below was causing an infinite loop
-                  // cardWidgets has to be cleared
-                  var cardWidgets = <Widget>[];
-                  //cardWidgets.clear(); //uncomment this line if you are declaring cardWidgets globally
-
-                  for (int i = 0; i < cardsList.length; i++) {
-                    CardKey cardKey = CardKey(
-                        position: i,
-                        cardName: cardsList[i],
-                        cardXY: playersCardsTransform);
-                    cardWidgets.add(card(cardKey));
-                  }
-
-                  if (!isStateChanged) {
-                    cardWidgetsBuilderList = cardWidgets;
-                  }
-
-                  print("infinite loop check");
-
-                  return SizedBox(
-                    height: 70,
-                    child: Center(
-                      child: ListView.builder(
-                        clipBehavior: Clip.none,
-                        shrinkWrap: true,
-                        scrollDirection: Axis.horizontal,
-                        itemCount: cardWidgetsBuilderList.length,
-                        itemBuilder: (context, index) {
-                          return cardWidgetsBuilderList[index];
-                        },
-                      ),
-                    ),
-                  );
-                } else {
-                  return const Text(
-                      "Something went wrong. Unable to retrieve cards.");
-                }
-              })),
-        ),
-      ]),
-    );
-  }
-
-  Widget card(CardKey cardKey) {
-    String cardName = cardKey.cardName!;
-    var _cardKey = ValueKey(cardKey);
-
-    return AnimatedContainer(
-      key: _cardKey,
-      transform: playersCardsTransform,
-      width: cardWidth,
-      height: cardHeight,
-      duration: const Duration(milliseconds: 200),
-      curve: Curves.fastOutSlowIn,
-      child: GestureDetector(
-        onTap: () {
-          if (tappedCards.length < 4) {
-            var result = cardWidgetsBuilderList
-                .indexWhere((element) => element.key == _cardKey);
-            setState(() {
-              isStateChanged = true;
-              tappedCards.add(cardWidgetsBuilderList[result]);
-              cardWidgetsBuilderList
-                  .removeWhere((element) => element.key == _cardKey);
-            });
-          }
-          // THIS CODE BELOW IS FOR ANIMATING THE CARDS ONTO THE TABLE
-          // ALSO THE ANIMTED CONTAINER HAD STATEFULL BUILDER WRAPPED AROUND IT!!!
-          //print("before: ${context.globalPaintBounds}");
-          // 170 because of the 50 bottom paddding (from Positioned of playerCards) and
-          // 50 for faceUpcard, and 70 for cardHeight
-          // double y = -(constrainHeight / 2) + 170;
-          // double x = 100;
-          // setState(() {
-          //
-          //   playersCardsTransform = Matrix4.translationValues(x, y, 0);
-          // });
-          // print("after: ${context.globalPaintBounds}");
-
-          //_moveCard();
-        },
-        child: Image.asset(
-          "assets/images/$cardName.png",
-          fit: BoxFit.cover,
-          width: cardWidth,
-          height: cardHeight,
-        ),
-      ),
-    );
-  }
-
-  _moveCard() {
-    setState(() {
-      playersCardsTransform = Matrix4.translationValues(0, -100, 0);
-    });
-  }
-
-  Future<List<String>> cardsSnapshot() async {
-    final snapshot =
-        await onceRef.child('tables/1/cards/playerCards/$uid/startingHand').get();
-    if (snapshot.exists) {
-      var cardsList = List<String>.from(snapshot.value as List);
-      return cardsList;
-    } else {
-      return [];
-    }
-  }
 
   Widget deck() {
     return Center(
@@ -360,194 +234,6 @@ class GamePageState extends State<GamePage> {
             )));
   }
 
-  Widget fiveCardBorders() {
-    return SafeArea(child: LayoutBuilder(
-        builder: ((BuildContext context, BoxConstraints constraints) {
-      return Stack(
-        children: [
-          // Red dot posiition 1
-          Positioned(
-              top: (constraints.constrainHeight() / 2) + 50,
-              left:
-                  (constraints.constrainWidth() / 2) - ((cardWidth * 2.5) + 10),
-              child: const SizedBox(
-                  height: 70,
-                  width: 50,
-                  child: Center(
-                    child: Icon(
-                      Icons.circle_sharp,
-                      color: Colors.red,
-                      size: 12,
-                    ),
-                  ))),
-
-          // Red dot posiition 2
-          Positioned(
-              top: (constraints.constrainHeight() / 2) + 50,
-              left:
-                  (constraints.constrainWidth() / 2) - ((cardWidth * 1.5) + 5),
-              child: const SizedBox(
-                  height: 70,
-                  width: 50,
-                  child: Center(
-                    child: Icon(
-                      Icons.circle_sharp,
-                      color: Colors.red,
-                      size: 12,
-                    ),
-                  ))),
-
-          // Red dot posiition 3
-          Positioned(
-              top: (constraints.constrainHeight() / 2) + 50,
-              left: (constraints.constrainWidth() / 2) - (cardWidth / 2),
-              child: const SizedBox(
-                  height: 70,
-                  width: 50,
-                  child: Center(
-                    child: Icon(
-                      Icons.circle_sharp,
-                      color: Colors.red,
-                      size: 12,
-                    ),
-                  ))),
-
-          // Red dot posiition 4
-          Positioned(
-              top: (constraints.constrainHeight() / 2) + 50,
-              left: (constraints.constrainWidth() / 2) + ((cardWidth / 2) + 5),
-              child: const SizedBox(
-                  height: 70,
-                  width: 50,
-                  child: Center(
-                    child: Icon(
-                      Icons.circle_sharp,
-                      color: Colors.red,
-                      size: 12,
-                    ),
-                  ))),
-
-          // Red dot posiition 5
-          Positioned(
-              top: (constraints.constrainHeight() / 2) + 50,
-              left:
-                  (constraints.constrainWidth() / 2) + ((cardWidth * 1.5) + 10),
-              child: const SizedBox(
-                  height: 70,
-                  width: 50,
-                  child: Center(
-                    child: Icon(
-                      Icons.circle_sharp,
-                      color: Colors.red,
-                      size: 12,
-                    ),
-                  ))),
-
-          // Card position #1 (faceUpCard)
-          Positioned(
-              top: (constraints.constrainHeight() / 2) + 50,
-              left:
-                  (constraints.constrainWidth() / 2) - ((cardWidth * 2.5) + 10),
-              child: Container(
-                height: 70,
-                width: 50,
-                // decoration: BoxDecoration(
-                //     border: Border.all(color: Colors.yellowAccent)),
-              )),
-
-          // Card position #2
-          AnimatedPositioned(
-              top: (constraints.constrainHeight() / 2) + 50,
-              left: playButtonSelected
-                  ? (constraints.constrainWidth() / 2) -
-                      ((cardWidth * 2.5) + 10)
-                  : (constraints.constrainWidth() / 2) -
-                      ((cardWidth * 1.5) + 5),
-              duration: tableCardAnimationDuration,
-              child: Container(
-                //margin: const EdgeInsets.all(15.0),
-                height: 70,
-                width: 50,
-                // decoration: BoxDecoration(
-                //     border: Border.all(color: Colors.yellowAccent)),
-                child: tableCard(0),
-              )),
-
-          // Card position #3
-          AnimatedPositioned(
-              top: (constraints.constrainHeight() / 2) + 50,
-              left: playButtonSelected
-                  ? (constraints.constrainWidth() / 2) -
-                      ((cardWidth * 2.5) + 10)
-                  : (constraints.constrainWidth() / 2) - (cardWidth / 2),
-              duration: tableCardAnimationDuration,
-              child: Container(
-                //margin: const EdgeInsets.all(15.0),
-                height: 70,
-                width: 50,
-                // decoration: BoxDecoration(
-                //     border: Border.all(color: Colors.yellowAccent)),
-                child: tableCard(1),
-              )),
-
-          // Card position #4
-          AnimatedPositioned(
-              top: (constraints.constrainHeight() / 2) + 50,
-              left: playButtonSelected
-                  ? (constraints.constrainWidth() / 2) -
-                      ((cardWidth * 2.5) + 10)
-                  : (constraints.constrainWidth() / 2) + ((cardWidth / 2) + 5),
-              duration: tableCardAnimationDuration,
-              child: Container(
-                //margin: const EdgeInsets.all(15.0),
-                height: 70,
-                width: 50,
-                // decoration: BoxDecoration(
-                //     border: Border.all(color: Colors.yellowAccent)),
-                child: tableCard(2),
-              )),
-
-          //Card position #5
-          AnimatedPositioned(
-              top: (constraints.constrainHeight() / 2) + 50,
-              left: playButtonSelected
-                  ? (constraints.constrainWidth() / 2) -
-                      ((cardWidth * 2.5) + 10)
-                  : (constraints.constrainWidth() / 2) +
-                      ((cardWidth * 1.5) + 10),
-              duration: tableCardAnimationDuration,
-              child: Container(
-                //margin: const EdgeInsets.all(15.0),
-                height: 70,
-                width: 50,
-                //decoration: BoxDecoration(
-                //border: Border.all(color: Colors.yellowAccent)),
-                child: tableCard(3),
-              )),
-        ],
-      );
-    })));
-  }
-
-  Widget? tableCard(int pos) {
-    if (tappedCards.asMap().containsKey(pos)) {
-      return Stack(
-        children: [
-          tappedCards[pos],
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                cardWidgetsBuilderList.add(tappedCards[pos]);
-                tappedCards.removeAt(pos);
-              });
-            },
-          )
-        ],
-      );
-    } else {
-      return null;
-    }
-  }
 
   Widget faceUpCard() {
     return SafeArea(child: LayoutBuilder(
@@ -579,60 +265,11 @@ class GamePageState extends State<GamePage> {
                   } else {
                     return const Text("game has not started");
                   }
-
-                  // THIS CODE IS FOR FUTUREBUILDER
-                  // if (snapshot.hasData) {
-                  //   faceUpCardName = snapshot.data.toString();
-                  //   return Image.asset(
-                  //     "assets/images/$faceUpCardName.png",
-                  //     width: cardWidth,
-                  //     height: cardHeight,
-                  //   );
-                  // } else {
-                  //   return Text(
-                  //       "An error occcured retrieving the face up card");
-                  // }
                 },
               ))
         ],
       );
     })));
-  }
-
-  Future<String> getFaceUpCard() async {
-    /**
-     // The following code kept throwing Bad State errors
-        DatabaseReference faceUpCardRef =
-        FirebaseDatabase.instance.ref('tables/1/cards/faceUpCard');
-        Completer<String> completer = Completer();
-
-        faceUpCardRef.onValue.listen((event) async {
-          var faceUpCardList = List<String>.from(event.snapshot.value as List);
-          //faceUpCardList = _faceUpCardList;
-          setState(() {
-            faceUpCardName = faceUpCardList[0];
-          });
-          completer.complete(faceUpCardList[0]);
-        }).onError((error) {
-          completer.completeError(error);
-        });
-
-        return completer.future;
-     */
-
-    DatabaseReference faceUpCardRef =
-        FirebaseDatabase.instance.ref('tables/1/cards/faceUpCard');
-
-    try {
-      var event = await faceUpCardRef.once();
-      var faceUpCardList = List<String>.from(event.snapshot.value as List);
-      setState(() {
-        faceUpCardName = faceUpCardList[0];
-      });
-      return faceUpCardList[0];
-    } catch (error) {
-      throw error;
-    }
   }
 
   Widget buttons() {
@@ -665,49 +302,49 @@ class GamePageState extends State<GamePage> {
   }
 
   playButton() async {
-    setState(() {
-      playButtonSelected = true;
-    });
-    List<String> cardsBeingPlayed = <String>[];
-    for (int i = 0; i < tappedCards.length; i++) {
-      ValueKey<CardKey> t = tappedCards[i].key! as ValueKey<CardKey>;
-      cardsBeingPlayed.add(t.value.cardName!);
-    }
+    // setState(() {
+    //   playButtonSelected = true;
+    // });
+    // List<String> cardsBeingPlayed = <String>[];
+    // for (int i = 0; i < tappedCards.length; i++) {
+    //   ValueKey<CardKey> t = tappedCards[i].key! as ValueKey<CardKey>;
+    //   cardsBeingPlayed.add(t.value.cardName!);
+    // }
 
-    DatabaseReference dbMoves = FirebaseDatabase.instance.ref('tables/1/moves');
-    DatabaseReference newMoves = dbMoves.push();
-    await newMoves.set({uid: cardsBeingPlayed}).then((value) {
-      setFaceUpCardAndHand(cardsBeingPlayed.last);
-      passPlay();
-    });
+    // DatabaseReference dbMoves = FirebaseDatabase.instance.ref('tables/1/moves');
+    // DatabaseReference newMoves = dbMoves.push();
+    // await newMoves.set({uid: cardsBeingPlayed}).then((value) {
+    //   setFaceUpCardAndHand(cardsBeingPlayed.last);
+    //   passPlay();
+    // });
   }
 
   setFaceUpCardAndHand(String card) async {
     // There is at least one card
-    if (cardWidgetsBuilderList.length > 0) {
-      List<String> cardsInHand = <String>[];
-      for (int i = 0; i < cardWidgetsBuilderList.length; i++) {
-        ValueKey<CardKey> t =
-            cardWidgetsBuilderList[i].key! as ValueKey<CardKey>;
-        cardsInHand.add(t.value.cardName!);
-      }
+    // if (cardWidgetsBuilderList.length > 0) {
+    //   List<String> cardsInHand = <String>[];
+    //   for (int i = 0; i < cardWidgetsBuilderList.length; i++) {
+    //     ValueKey<CardKey> t =
+    //         cardWidgetsBuilderList[i].key! as ValueKey<CardKey>;
+    //     cardsInHand.add(t.value.cardName!);
+    //   }
 
-      DatabaseReference faceUpCardRef =
-          FirebaseDatabase.instance.ref('tables/1/cards');
+    //   DatabaseReference faceUpCardRef =
+    //       FirebaseDatabase.instance.ref('tables/1/cards');
 
-      await faceUpCardRef.update(
-          {'faceUpCard': card, 'playerCards/$uid/startingHand': cardsInHand}).then((value) {
-        Future.delayed(const Duration(milliseconds: 500), () {
-          setState(() {
-            playButtonSelected = false;
-            tappedCards.clear();
-          });
-        });
-      });
-    } else {
-      // No cards left, you are the winner
+    //   await faceUpCardRef.update(
+    //       {'faceUpCard': card, 'playerCards/$uid/startingHand': cardsInHand}).then((value) {
+    //     Future.delayed(const Duration(milliseconds: 500), () {
+    //       setState(() {
+    //         playButtonSelected = false;
+    //         tappedCards.clear();
+    //       });
+    //     });
+    //   });
+    // } else {
+    //   // No cards left, you are the winner
 
-    }
+    // }
   }
 
   passPlay() async {
@@ -745,37 +382,18 @@ class GamePageState extends State<GamePage> {
     await cardsRef
         .update({"dealer/deck": deck, "playerCards/$uid/startingHand": playersCards});
 
-    setState(() {
-      isStateChanged = true;
-      cardWidgetsBuilderList.add(card(CardKey(
-          position: cardWidgetsBuilderList.length,
-          cardName: cardBeingAdded,
-          cardXY: playersCardsTransform)));
-    });
+    // TODO: only disablinf for now
+    // setState(() {
+    //   isStateChanged = true;
+    //   cardWidgetsBuilderList.add(card(CardKey(
+    //       position: cardWidgetsBuilderList.length,
+    //       cardName: cardBeingAdded,
+    //       cardXY: playersCardsTransform)));
+    // });
   }
 
   ranOutOfTime() async {
     await addCard();
     await passPlay();
   }
-}
-
-class CardKey {
-  int? position;
-  String? cardName;
-  Matrix4? cardXY;
-
-  CardKey(
-      {required this.position, required this.cardName, required this.cardXY});
-
-  CardKey copyWith({int? position, String? cardName, Matrix4? cardXY}) {
-    return CardKey(
-        position: position ?? this.position,
-        cardName: cardName ?? this.cardName,
-        cardXY: cardXY ?? this.cardXY);
-  }
-
-  @override
-  String toString() =>
-      'CardKeys(position: $position, cardName: $cardName, cardXY: $cardXY)';
 }
