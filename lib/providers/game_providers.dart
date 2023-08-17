b63a028211188d37a60f00b02d1776d9dcff7218
almanza1112 <almanza1112@gmail.com>
2023-08-16 23:49:23 -0400

    betting adding

--- lib/providers/game_providers.dart
+++ lib/providers/game_providers.dart
@@ -42,6 +42,6 @@ final brickCardSuitPositionProvider = StateProvider<int>((ref) => 0);
 
 final brickCardNumberPositionProvider = StateProvider<int>((ref) => 0);
 
-final chipsSliderValueProvider = StateProvider<double>((ref) => 0);
+final chipsValueProvider = StateProvider<double>((ref) => 0);
 
 
