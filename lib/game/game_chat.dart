b7fb6cd26222dd03d828026799b5610e3c3a5da7
almanza1112 <almanza1112@gmail.com>
2024-01-07 20:26:25 -0500

    game chat optimized

--- lib/game/game_chat.dart
+++ lib/game/game_chat.dart
@@ -24,158 +24,202 @@ class GameChatState extends ConsumerState<GameChat> {
   final uid = FirebaseAuth.instance.currentUser!.uid;
   final TextEditingController _messageController = TextEditingController();
 
+  late ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    // Scroll to the bottom when the widget is first built
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
+      _scrollToBottom();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      backgroundColor: Colors.black,
-      child: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 12),
-          child: Column(
-            children: [
-              ListTile(
-                contentPadding: const EdgeInsets.only(top: 4, bottom: 16),
-                title: const Text(
-                  "CHAT",
-                  style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ),
-                trailing: IconButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  icon: const Icon(Icons.close, color: Colors.white54),
-                ),
-              ),
-              Expanded(
-                  child: StreamBuilder(
-                      stream: chatRef.onValue,
-                      builder: ((context, snapshot) {
-                        if (snapshot.hasData &&
-                            snapshot.data!.snapshot.value != null) {
-                          List<Message> messages = [];
+    return Column(
+      children: [
+        Expanded(
+          child: Drawer(
+            backgroundColor: Colors.black,
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 12),
+                child: Column(
+                  children: [
+                    ListTile(
+                      contentPadding: const EdgeInsets.only(top: 4, bottom: 16),
+                      title: const Text(
+                        "CHAT",
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                      trailing: IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: const Icon(Icons.close, color: Colors.white54),
+                      ),
+                    ),
+                    Expanded(
+                      child: StreamBuilder(
+                          stream: chatRef.onValue,
+                          builder: ((context, snapshot) {
+                            if (snapshot.hasData &&
+                                snapshot.data!.snapshot.value != null) {
+                              List<Message> messages = [];
 
-                          Map<dynamic, dynamic> data = snapshot
-                              .data!.snapshot.value as Map<dynamic, dynamic>;
+                              Map<dynamic, dynamic> data = snapshot.data!
+                                  .snapshot.value as Map<dynamic, dynamic>;
 
-                          data.forEach((key, value) {
-                            var message = Message(
-                                uid: value['playerInfo']['uid'],
-                                position: value['position'],
-                                text: value['text'],
-                                photoURL: value['playerInfo']['photoURL'],
-                                username: value['playerInfo']['username'],
-                                timestamp: DateTime.fromMillisecondsSinceEpoch(
-                                    value['timestamp']));
+                              data.forEach((key, value) {
+                                var message = Message(
+                                    uid: value['playerInfo']['uid'],
+                                    position: value['position'],
+                                    text: value['text'],
+                                    photoURL: value['playerInfo']['photoURL'],
+                                    username: value['playerInfo']['username'],
+                                    timestamp:
+                                        DateTime.fromMillisecondsSinceEpoch(
+                                            value['timestamp']));
 
-                            messages.add(message);
-                          });
+                                messages.add(message);
+                              });
 
-                          messages.sort(
-                              (a, b) => a.timestamp.compareTo(b.timestamp));
+                              messages.sort(
+                                  (a, b) => a.timestamp.compareTo(b.timestamp));
+                                  WidgetsBinding.instance
+                                  .addPostFrameCallback((_) {
+                                _scrollToBottom();
+                              });
 
-                          return ListView.builder(
-                              itemCount: messages.length,
-                              itemBuilder: ((context, index) {
-                                return Padding(
-                                  padding:
-                                      const EdgeInsets.only(top: 4, bottom: 4),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        messages[index].uid == uid
-                                            ? MainAxisAlignment.end
-                                            : MainAxisAlignment.start,
-                                    children: <Widget>[
-                                      if (messages[index].uid != uid)
-                                        CircleAvatar(
-                                          backgroundImage: NetworkImage(
-                                              messages[index].photoURL!),
-                                          radius: 20,
-                                        ),
-                                      Column(
-                                        crossAxisAlignment:
-                                            messages[index].uid == uid
-                                                ? CrossAxisAlignment.end
-                                                : CrossAxisAlignment.start,
+                              return ListView.builder(
+                                  controller: _scrollController,
+                                  //shrinkWrap: true,
+                                  itemCount: messages.length,
+                                  itemBuilder: ((context, index) {
+                                    return Padding(
+                                      padding: const EdgeInsets.only(
+                                          top: 4, bottom: 4),
+                                      child: Row(
                                         mainAxisAlignment:
-                                            MainAxisAlignment.center,
+                                            messages[index].uid == uid
+                                                ? MainAxisAlignment.end
+                                                : MainAxisAlignment.start,
                                         children: <Widget>[
                                           if (messages[index].uid != uid)
-                                            Text(
-                                              messages[index].username!,
-                                              style: const TextStyle(
-                                                  color: Colors.amber,
-                                                  fontSize: 10,
-                                                  fontWeight: FontWeight.w300),
+                                            CircleAvatar(
+                                              backgroundImage: NetworkImage(
+                                                  messages[index].photoURL!),
+                                              radius: 20,
                                             ),
-                                          Container(
-                                            decoration: BoxDecoration(
-                                                color:
-                                                    messages[index].uid == uid
-                                                        ? Colors.white
-                                                        : Colors.blue,
-                                                borderRadius:
-                                                    const BorderRadius.all(
-                                                        Radius.circular(20))),
-                                            child: Padding(
-                                              padding: const EdgeInsets.only(
-                                                  left: 6,
-                                                  right: 6,
-                                                  top: 2,
-                                                  bottom: 2),
-                                              child: Text(
-                                                messages[index].text,
-                                                style: TextStyle(
+                                          Column(
+                                            crossAxisAlignment:
+                                                messages[index].uid == uid
+                                                    ? CrossAxisAlignment.end
+                                                    : CrossAxisAlignment.start,
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: <Widget>[
+                                              if (messages[index].uid != uid)
+                                                Text(
+                                                  messages[index].username!,
+                                                  style: const TextStyle(
+                                                      color: Colors.amber,
+                                                      fontSize: 10,
+                                                      fontWeight:
+                                                          FontWeight.w300),
+                                                ),
+                                              Container(
+                                                decoration: BoxDecoration(
                                                     color:
                                                         messages[index].uid ==
                                                                 uid
+                                                            ? Colors.white
+                                                            : Colors.blue,
+                                                    borderRadius:
+                                                        const BorderRadius.all(
+                                                            Radius.circular(
+                                                                20))),
+                                                child: Padding(
+                                                  padding:
+                                                      const EdgeInsets.only(
+                                                          left: 6,
+                                                          right: 6,
+                                                          top: 2,
+                                                          bottom: 2),
+                                                  child: Text(
+                                                    messages[index].text,
+                                                    style: TextStyle(
+                                                        color: messages[index]
+                                                                    .uid ==
+                                                                uid
                                                             ? Colors.black
                                                             : Colors.white),
-                                              ),
-                                            ),
-                                          )
+                                                  ),
+                                                ),
+                                              )
+                                            ],
+                                          ),
                                         ],
                                       ),
-                                    ],
-                                  ),
-                                );
-                              }));
-                        } else {
-                          return const Center(
-                            child: Text(
-                              "No messages yet",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          );
-                        }
-                      }))),
-              Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: _messageController,
-                      decoration: const InputDecoration(
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.zero)),
-                          hintText: "Send Message"),
+                                    );
+                                  }));
+                            } else {
+                              return const Center(
+                                child: Text(
+                                  "No messages yet",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              );
+                            }
+                          })),
                     ),
-                  ),
-                  IconButton(
-                      onPressed: sendMessage,
-                      icon: const Icon(
-                        Icons.send,
-                        color: Colors.green,
-                      ))
-                ],
-              )
-            ],
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                            child: TextField(
+                              controller: _messageController,
+                              decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: Colors.white,
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(30)),
+                                  hintText: "Send Message"),
+                            ),
+                          ),
+                        ),
+                        IconButton(
+                            onPressed: sendMessage,
+                            icon: const Icon(
+                              Icons.send,
+                              color: Colors.green,
+                            ))
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
           ),
         ),
-      ),
+        SizedBox(
+          height: MediaQuery.of(context).viewInsets.bottom,
+        )
+      ],
+    );
+  }
+
+  void _scrollToBottom() {
+    _scrollController.animateTo(
+      _scrollController.position.maxScrollExtent,
+      duration: Duration(milliseconds: 250),
+      curve: Curves.easeInOut,
     );
   }
 
